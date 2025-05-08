949ce7dacdd224a4317aae427840a40fb90e8278
Jorge Menezes Jr <jgrei.junior@gmail.com>
2025-05-08 09:00:10 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,26 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const rentPrice = 40;
+  const fullDescont = 50;
+  const lessDescont = 20;
+  const minDay = 3;
+  const maxDay = 7;
+  let rentAmount = 0;
+
+  if (days >= maxDay) {
+    rentAmount = rentPrice * days - fullDescont;
+
+    return rentAmount;
+  }
+
+  if (days >= minDay) {
+    rentAmount = rentPrice * days - lessDescont;
+
+    return rentAmount;
+  }
+
+  return (rentAmount = rentPrice * days);
 }
 
 module.exports = calculateRentalCost;
